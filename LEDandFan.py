aca1569e2ac64c969977669442481041f0d7e4a9
I'm Pen <88584656+Pencoding1@users.noreply.github.com>
2022-11-30 23:48:36 +0700

    1

--- LEDandFan.py
+++ LEDandFan.py
@@ -1,12 +1,11 @@
 from yolobit import *
-button_a.on_pressed = None
-button_b.on_pressed = None
-button_a.on_pressed_ab = button_b.on_pressed_ab = -1
 from homebit3_rgbled import RGBLed
 import time
+from mqtt import *
 
 tiny_rgb = RGBLed(pin1.pin, 4)
 tiny_rgb2 = RGBLed(pin10.pin, 4)
+auto = True
 # round(translate((pin0.read_analog()), 0, 4095, 0, 100)): light sensor
 # light_level(): light sensor
 # pin10.write_analog(round(translate(70,0,100,0,1023))): Fan
@@ -20,16 +19,28 @@ tiny_rgb2 = RGBLed(pin10.pin, 4)
 #     pin10.write_analog(round(translate(70, 0, 100, 0, 1023)))
 #   elif temperature() == 0:
 #     pass
+def on_mqtt_message_receive_callback__auto_(info):
+  """Điều khiển IOT"""
+  global auto
+  if info == "1":
+    auto = True
+  else:
+    auto = False
+  
+
+
 def light():
   print(light_level(),":1")
-  if light_level() <= 20:
+  # if light_level() <= 20:
+  if True:
     tiny_rgb.show(0, hex_to_rgb('#ffffff'))
   else:
     tiny_rgb.show(0, hex_to_rgb('#000000'))
 
 def light2():
   print(round(translate((pin0.read_analog()), 0, 4095, 0, 100)),":2")
-  if round(translate((pin0.read_analog()), 0, 4095, 0, 100)) <= 20:
+  # if round(translate((pin0.read_analog()), 0, 4095, 0, 100)) <= 20:
+  if True:
     tiny_rgb2.show(0, hex_to_rgb('#ffffff'))
     tiny_rgb.show(0, hex_to_rgb('#ffffff'))
   else:
@@ -38,13 +49,31 @@ def light2():
 
 def fan():
   print(temperature())
-  if 25 <= round(temperature()) <= 28:
+  # if 25 <= round(temperature()) <= 28:
+  if True:
+    pin14.write_analog(round(translate(70, 0, 100, 0, 1023)))
+  elif round(temperature()) > 32:
+    pin14.write_analog(round(translate(100, 0, 100, 0, 1023)))
+
+def fan2():
+  print(temperature())
+  # if 25 <= round(temperature()) <= 28:
+  if True:
     pin2.write_analog(round(translate(70, 0, 100, 0, 1023)))
   elif round(temperature()) > 32:
-    pin2.write_analog(round(translate(100, 0, 100, 0, 1023)))
-if True:
-  pass
+    pin2.write_analog(round(translate(0, 0, 100, 0, 1023)))
 
-while True:
-  light2()
-  time.sleep_ms(1000)
+
+if __name__ == "__main__":
+  if True:
+      mqtt.connect_wifi('LMHT', '0916628391')
+      mqtt.connect_broker(server='io.adafruit.com', port=1883, username='Pen215', password='aio_LmGo95rUINBj0MRsqTIOMEKHoaWo')
+      mqtt.on_receive_message('auto', on_mqtt_message_receive_callback__auto_)
+  
+  while True:
+    mqtt.check_message()
+    fan()
+    fan2()
+    light2()
+    light()
+    time.sleep_ms(1000)
--- door.py
+++ door.py
@@ -4,9 +4,6 @@ import urequests
 import gc
 import time
 from yolobit import *
-button_a.on_pressed = None
-button_b.on_pressed = None
-button_a.on_pressed_ab = button_b.on_pressed_ab = -1
 import sys
 import uselect
 from homebit3_rgbled import RGBLed
@@ -14,8 +11,12 @@ from homebit3_rgbled import RGBLed
 tiny_rgb = RGBLed(pin0.pin, 4)
 TOKEN = "5955181205:AAHsgeilXg28sIMwDXmc77wmiQgYro3eN9I"
 ID = "-895508981"
+Wifi = "LMHT"
+password = "0916628391"
+
 
 def read_terminal_input():
+  """Đọc thông tin từ camera"""
   spoll=uselect.poll()        # Set up an input polling object.
   spoll.register(sys.stdin, uselect.POLLIN)    # Register polling object.
 
@@ -32,6 +33,7 @@ def read_terminal_input():
 
 delay_sending = 20
 def sending_mess(mode):
+  """Gửi tin nhắn về telegram"""
   global delay_sending, sending_excution_time
   sending_time = 0
   
@@ -41,7 +43,7 @@ def sending_mess(mode):
       http_response = urequests.get((''.join([str(x) for x in ['https://api.telegram.org/bot', TOKEN, '/sendMessage?text=', 'có người lạ đứng trước cửa', '&chat_id=', ID]])))
       http_response.close()
       sending_time = (round(time.time()) - start)
-      print(f"sending_time: {sending_time}")
+      # print(f"sending_time: {sending_time}")
     
     elif mode == 2:
       return
@@ -51,30 +53,34 @@ def sending_mess(mode):
       http_response = urequests.get((''.join([str(x) for x in ['https://api.telegram.org/bot', TOKEN, '/sendMessage?text=', 'có người vào nhà', '&chat_id=', ID]])))
       http_response.close()
       sending_time = (round(time.time()) - start)
-      print(f"sending_time: {sending_time}")
+      # print(f"sending_time: {sending_time}")
     
     delay_sending = 0
   
   delay_sending += (1 + sending_time)
   sending_excution_time = sending_time
   sending_time = 0
-  print("delay_sending: {}".format(delay_sending))
+  # print("delay_sending: {}".format(delay_sending))
 
 
 def PIR(cam=None, off=None):
-  if pin20.read_digital()!= 1:
-  
-    if not cam:
-      sending_mess(1)
-  
-    elif cam == "NL":
-      sending_mess(1)
-    else:
-      return
-
+  """Điều khiển PIR"""
+  if not off:
+    if pin20.read_digital()!= 1:
+    
+      if not cam:
+        sending_mess(1)
+    
+      elif cam == "NL":
+        sending_mess(1)
+      else:
+        return
+  else:
+    return
 
 
 def cam():
+  """Nhận diện khuôn mặt"""
   global cam_input
   cam_input = "NQ"
   
@@ -84,13 +90,12 @@ def cam():
   elif cam_input == 'NL':
     return "NL"
   
-  print(cam_input)
+  # print(cam_input)
   return False
-  
-  
 
-# Khóa cửa
+
 def door():
+  """Hệ thống cửa tự động"""
   global delay_sending
   pin4.servo_write(60)
   time.sleep_ms(5000)
@@ -99,6 +104,7 @@ def door():
 
 fail, counting, delay = 0, 0, 5
 def security_system(permision):
+  """Hệ thống bảo mật của cửa"""
   global fail, delay,counting, delay_sending, sending_excution_time
   locked = False
   sending = True
@@ -119,9 +125,9 @@ def security_system(permision):
     locked = False
     counting = 0
     
-    print(fail)
+    # print(fail)
   
-  print(f"counting: {counting}, fail: {fail}, locked: {locked}")
+  # print(f"counting: {counting}, fail: {fail}, locked: {locked}")
     
   if not locked and delay>=5:
 
@@ -131,22 +137,35 @@ def security_system(permision):
     elif permision == "NL":
       fail += 1
     delay = 0
-  print(delay)
+  # print(delay)
   delay += 1
+  
+
+def on_mqtt_message_receive_callback__work_please_(info):
+  """Điều khiển IOT"""
+  global delay
+  if info == "1":
+    PIR(off=True)
+    door()
+    PIR(off=False)
+    delay -= 2
+  else:
+    return
   
   
 if __name__ == "__main__":
   if True:
-    wifi.connect_wifi('LMHT', '0916628391')
-    mqtt.connect_wifi('LMHT', '0916628391')
-    mqtt.connect_broker(server='io.adafruit.com', port=1883, username='Pen215', password='aio_MJpt91Ige2Y8WtNHLeXQr7wgbsQB')
+    wifi.connect_wifi(Wifi, password)
+    mqtt.connect_wifi(Wifi, password)
+    mqtt.connect_broker(server='io.adafruit.com', port=1883, username='Pen215', password='aio_LmGo95rUINBj0MRsqTIOMEKHoaWo')
+    mqtt.on_receive_message('work-please', on_mqtt_message_receive_callback__work_please_)
     gc.collect()
 
   while True:
-    mqtt.check_message()
-    sending_mess(2)
-    camera = cam()
-    security_system(camera)
-    PIR(cam=camera)
+    if not turn_off:
+      mqtt.check_message()
+      sending_mess(2)
+      camera = cam()
+      security_system(camera)
+      PIR(cam=camera)
     time.sleep_ms(1000)
-  
